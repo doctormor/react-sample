482cd8e0016fac87c8ad63c698b297fb30ce4d43
doctormor <45977725+doctormor@users.noreply.github.com>
2021-02-18 22:00:41 +0700

    Add files via upload

---
+++ build/precache-manifest.9831dc5dd4c41e2c6fe328823954806b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b12c08f13bc8fbd75458a31ef91134a",
+    "url": "/ondev/index.html"
+  },
+  {
+    "revision": "76062b5f3d2e5b136d3e",
+    "url": "/ondev/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "dcdc5ad487ac7fa01cbf",
+    "url": "/ondev/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "76062b5f3d2e5b136d3e",
+    "url": "/ondev/static/js/2.f25a3f92.chunk.js"
+  },
+  {
+    "revision": "f2efbe82e3ec6b086295954b52317f8c",
+    "url": "/ondev/static/js/2.f25a3f92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcdc5ad487ac7fa01cbf",
+    "url": "/ondev/static/js/main.2b446c49.chunk.js"
+  },
+  {
+    "revision": "5e02aefadee47895d06d",
+    "url": "/ondev/static/js/runtime-main.e9cdecfb.js"
+  }
+]);
